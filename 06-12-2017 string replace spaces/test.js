18df4c78c524a2e57b2f1db5be50a8bd8bfbb9c9
SuRu <su33.dev@gmail.com>
2017-06-12 11:29:28 -0500

    string replace spaces

---
+++ 06-12-2017 string replace spaces/test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by SuRu on 06/12/2017.
+ */
+var data = "This Is Sample Data";
+console.log(data.replace(/\s/g, ''));
---
+++ 06-12-2017 string replace spaces/test.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by SuRu on 06/12/2017.
+ */
+
+var data: string = "This Is Sample Data";
+console.log(data.replace(/\s/g, ''));
